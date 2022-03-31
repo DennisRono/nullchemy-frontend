7bc233ebe8f1062539d3c59454b800ed96c5d384
DennisRono <finnneron1@gmail.com>
2022-03-31 17:45:01 +0300

    compound cloud better behavior willing

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,10 +4,23 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import Home from './Routes/Home'
+import About from './Routes/About'
 
 const Routes = () => {
   return (
-    <div>Routes</div>
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="teams" element={<Teams />}>
+            <Route path=":teamId" element={<Team />} />
+            <Route path="new" element={<NewTeamForm />} />
+            <Route index element={<LeagueStandings />} />
+            </Route>
+        </Route>
+        </Routes>
+    </BrowserRouter>,
   )
 }
 
